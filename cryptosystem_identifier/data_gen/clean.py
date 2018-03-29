0e152210c5b77b77c5b5a94d1781972e45249ffb
Arshdeep Singh <arshdeepiitd@gmail.com>
2018-03-29 14:50:23 +0530

    chaneg

--- cryptosystem_identifier/data_gen/clean.py
+++ cryptosystem_identifier/data_gen/clean.py
@@ -1,18 +1,18 @@
-N = 50 # Number of Training examples needed
+N = 500 # Number of Training examples needed
 
 import re
 
-rf = open("got.txt")
-text = rf.read()
-clean_text = ''.join(re.findall('[A-Za-z ]', text))
-clean_text = re.sub(' +', ' ', clean_text)
-clean_text = clean_text.upper()
+# rf = open("got.txt")
+# text = rf.read()
+# clean_text = ''.join(re.findall('[A-Za-z ]', text))
+# clean_text = re.sub(' +', ' ', clean_text)
+# clean_text = clean_text.upper()
 
 # print(clean_text[:100])
-wf = open("corpus.txt", "w")
-wf.write(clean_text)
+wf = open("./text/corpus.txt", "r")
+# wf.write(clean_text)
 
-wf = open("plain_text.txt", "w")
+wf = open("./train/train_plain_text_long.txt", "w")
 vig = open("vig.txt", "w")
 ss = open("simple_sub.txt", "w")
 
