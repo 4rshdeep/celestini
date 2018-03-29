e3a2fd61efd37460fd034929e5db51d667dcd74f
Mayank Singh Chauhan <mayanksingh2298@gmail.com>
2018-03-29 20:54:39 +0530

    convolution added

--- programming1/sparse_mul.py
+++ programming1/sparse_mul.py
@@ -10,8 +10,9 @@ def mat_to_sparse(mat):
 
 
 #these are the matrices to multiply
-mat1 = [[1,0,2],[2,1,2]]
-mat2 = [[1,1],[0,2],[3,1]]
+mat1 = [[0,2],[2,0]]
+mat2 = [[1,1],[0,2]]
+mat2org=mat2
 
 #get the transpose of mat2
 mat2t=[]
@@ -26,6 +27,7 @@ mat2 = mat_to_sparse(mat2t)
 # print mat1
 # print mat2
 
+# Multiplication
 mulMat = []
 for i in range(len(mat1)):
 	mulMat.append([])
@@ -44,13 +46,33 @@ for i in range(len(mat1)):
 				b=b+1
 		if sum!=0:
 			mulMat[i].append((j,sum))
-print mulMat
+print "Sparse multiplication: ",mulMat
+
+# Convolution
+mat2org = mat_to_sparse(mat2org)
+# print mat2org
+sum=0
+for i in range(len(mat1)):
+	a=0
+	b=0
+	while(a<len(mat1[i]) and b<(len(mat2org[i]))):
+		if(mat1[i][a][0]==mat2org[i][b][0]):
+			sum=sum+mat1[i][a][1]*mat2org[i][b][1]
+			a=a+1
+			b=b+1
+		elif(mat1[i][a][0]<mat2org[i][b][0]):
+			a=a+1
+		else:
+			b=b+1
+print "Convolution: ",[sum]
 
 # what is the time complexity?
 # O(N*max(m1,m2))
 # what we do is that for every i,j in N*N, we multiply ith row of matrix1 with jth row of matrix2
 # so order is N*N*no of elements in each row
 # no of elements in each row are roughly m1/N and m2/N respectively
+# To calculate the convolution, we went to each nonzero number and multiplied them if they shared the same row and column
+# which again had the complexity max(m1,m2)
 
 # what is the space complexity?
 # we generated a matrix to store that non zero elements so the space complexity is max(m1,m2)
